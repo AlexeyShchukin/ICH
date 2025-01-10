3bed01d40b80d5358a9cd5be8b54f0deb21a6a75
yusuf <shchukin.developer@gmail.com>
2025-01-10 09:43:04 +0100

    Calculator

---
+++ python/hw3_task1.py
@@ -0,0 +1,36 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def sum_nums(self):
+        return self.a + self.b
+
+    def sub_nums(self):
+        return self.a - self.b
+
+    def prod_nums(self):
+        return self.a * self.b
+
+    def div_nums(self):
+        if self.b == 0:
+            return 'На ноль делить нельзя'
+        return self.a / self.b
+
+    def div_rest(self):
+        if self.b == 0:
+            return 'На ноль делить нельзя'
+        return self.a % self.b
+
+    def num_power(self):
+        return self.a ** self.b
+
+
+if __name__ == '__main__':
+    calculator = Calculator(float(input('Enter first number: ')), float(input('Enter second number: ')))
+    print("Sum:", calculator.sum_nums())
+    print("Difference:", calculator.sub_nums())
+    print("Product:", calculator.prod_nums())
+    print("Quotient:", calculator.div_nums())
+    print("Remainder from division:", calculator.div_rest())
+    print("The first number to the power of the second number:", calculator.num_power())
