6a55243327d0d25a323d0dba6e907505bd847a53
yusuf <shchukin.developer@gmail.com>
2025-01-13 12:19:01 +0100

    function changed

--- python/hw5_task2.py
+++ python/hw5_task2.py
@@ -1,9 +1,7 @@
 def bissextile(year: int):
-    if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
-        print('Год является високосным.')
-    else:
-        print('Год не является високосным.')
+    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 
 
 if __name__ == '__main__':
-    bissextile(int(input('Введите год: ')))
+    year = int(input('Enter year: '))
+    print(f'The year {"is" if bissextile(year) else "is not"} a leap year.')
