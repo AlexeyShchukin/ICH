3c3bba03b02c4b275509b58088ce4729ae3bfac2
yusuf <shchukin.developer@gmail.com>
2025-01-17 10:31:58 +0100

    fibonacci

--- python/hw6_task2.py
+++ python/hw6_task2.py
@@ -1,8 +1,13 @@
-n = int(input('Введите число N: '))
-prev_num = 0
-next_num = 1
-idx = 0
-while idx < n:
-    print(prev_num, end=', ' if idx < n - 1 else '')
-    prev_num, next_num = next_num, prev_num + next_num
-    idx += 1
+def fibonacci(num: int):
+    prev_num = 0
+    next_num = 1
+    idx = 0
+    while idx < num:
+        idx += 1
+        print(prev_num, end=', ' if idx < num else '\n')
+        prev_num, next_num = next_num, prev_num + next_num
+
+
+if __name__ == '__main__':
+    number = int(input('Enter number: '))
+    fibonacci(number)
