52a86384d22be87d850d7b2e8032ac31b444ed29
AlexeyShchukin <shchukin.developer@gmail.com>
2025-02-13 10:38:43 +0100

    pascal triangle with list

---
+++ practicum/pascal_triangle2.py
@@ -0,0 +1,31 @@
+"""Во входном файле задаётся число.
+Необходимо в выходной файл вывести треугольник Паскаля,
+который будет занимать верхний левый угол.
+Воспользуйтесь расчётом биномиального коэффициента в цикле.
+ВАЖНО: Деление одного факториала на произведение двух других ВСЕГДА
+даёт целочисленный результат!
+"""
+from math import factorial as f
+
+
+def bk(n, k):
+    """Расчёт биномиального коэффициента"""
+    return f(n) // (f(k) * f(n - k))
+
+
+def pascal_triangle(num):
+    with open('pascal_triangle.txt', 'w', encoding='utf-8') as pt:
+        longest = len(str(bk(num - 1, num // 2))) + 2  # длина самого большого числа в треугольнике
+        for i in range(num):
+            line = []
+            for j in range(i + 1):
+                line.append(
+                    f'{bk(i, j):^{longest}d}')  # центрируем числа по длине наибольшего числа и собираем в список
+            print(''.join(line).center(longest * num),
+                  file=pt)  # преобразуем список в строку и центрируем её по длине наибольшей строки
+
+
+with open('num.txt', 'r') as file:
+    number = int(file.read().strip())  # считываем число из первого файла
+
+pascal_triangle(number)
