033ce71664f96ccf1006df50cdb713d6a99c422a
AlexeyShchukin <shchukin.developer@gmail.com>
2025-02-18 16:25:41 +0100

    get value from dict

---
+++ python/hw21_task3.py
@@ -0,0 +1,18 @@
+def get_value_from_dict(d: dict, k):
+    if input('Use the get method? (y/n): ') == 'y':
+        return d.get(k)
+    return d.setdefault(k)
+
+
+def get_dict():
+    n = int(input('Enter a number of keys for your dictionary: '))
+    d = {}
+    for i in range(1, n + 1):
+        key = input(f'Enter the {i} key: ')
+        d[key] = input(f'Enter the value of the {i} key: ')
+    return d
+
+
+my_dict = get_dict()
+search_key = input('Enter search key: ')
+print(f'The value of key {search_key}: {get_value_from_dict(my_dict, search_key)}')
