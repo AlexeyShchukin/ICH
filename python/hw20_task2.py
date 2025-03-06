6a577d15d0ceb2a99a8cab19e7310b37f0991167
AlexeyShchukin <shchukin.developer@gmail.com>
2025-03-06 12:54:14 +0100

    hw 20 task 2

--- python/hw20_task2.py
+++ python/hw20_task2.py
@@ -1,8 +1,13 @@
 def count_unique_chars(s: str) -> int:
     chars = {}
     for char in s:
-        chars[char] = chars.get(char, 0) + 1
+        chars[char] = chars.get(char, 0) + 1  # записываем в хеш таблицу символы с количеством их повторений
     return len(chars)
 
 
+def count_unique_chars2(s: str) -> int:
+    return len(set(s))
+
+
 print(f"Number of unique characters: {count_unique_chars(input('Enter a string: '))}")
+print(f"Number of unique characters: {count_unique_chars2(input('Enter a string: '))}")
