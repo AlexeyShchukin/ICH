04917c9a79c6698068b51ecd0af886dafb88564d
AlexeyShchukin <shchukin.developer@gmail.com>
2025-03-14 12:31:54 +0100

    refactored hw22_task2, added nums.txt

--- python/hw22_task2.py
+++ python/hw22_task2.py
@@ -1,15 +1,17 @@
 try:
-    f = open(input("Enter the path to your file: "), 'r')
-except FileNotFoundError:
-    print('FileNotFoundError: Invalid path or file name')
+    f = open("nums.txt", 'r')
+except FileNotFoundError as e:
+        print(f'{e.__class__.__name__}: Invalid path or file name')
 else:
     a, b = f.read().strip().split()
     try:
         c = int(a) / int(b)
-    except ValueError:
-        print("ValueError: These are not integers")
-    except ZeroDivisionError:
-        print('ZeroDivisionError: Division by zero is not allowed')
+    except ValueError as e:
+        print(f'{e.__class__.__name__} These are not integers')
+    except ZeroDivisionError as e:
+        print(f'{e.__class__.__name__}: Division by zero is not allowed')
+    except Exception as e:
+        print(f'2. {e.__class__.__name__}: {e}')
     else:
         print(c)
     finally:
